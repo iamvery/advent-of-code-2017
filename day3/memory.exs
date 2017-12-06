4bf400ce8a50a5672a57ed4d86cf03974251739f
Jay Hayes <ur@iamvery.com>
2017-12-06 16:04:34 -0600

    wip [ci skip]

--- day3/memory.exs
+++ day3/memory.exs
@@ -3,7 +3,7 @@ defmodule Memory do
 
   # https://math.stackexchange.com/a/163101
   def coordinate(n) do
-    k = ceil((sqrt(n)-1)/2)
+    k = ceil((sqrt(n)-1)/2) |> round
     t = 2*k+1
     m = t*t
     t = t-1
@@ -25,6 +25,13 @@ defmodule Memory do
     end
   end
 
+  def spiral(_, _ \\ [])
+  def spiral(0, l), do: l
+  def spiral(n, l) do
+    coord = coordinate(n)
+    spiral(n-1, [coord | l])
+  end
+
   # https://en.wikipedia.org/wiki/Taxicab_geometry
   def distance(n) do
     {x, y} = coordinate(n)
@@ -42,6 +49,12 @@ defmodule MemoryTest do
     assert Memory.coordinate(11) == {2.0, 0.0}
     assert Memory.coordinate(18) == {-2.0, 1.0}
     assert Memory.coordinate(22) == {-1.0, -2.0}
+  end
+
+  test "spiral" do
+    assert Memory.spiral(1) == [{0,0}]
+    assert Memory.spiral(2) == [{0,0}, {1,0}]
+    assert Memory.spiral(3) == [{0,0}, {1,0}, {1,1}]
   end
 
   test "example 1" do
